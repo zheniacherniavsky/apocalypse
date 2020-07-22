fc6e5efe2723346f26460f037cd3287b86684dbd
geshka <62078242+zheniacherniavsky@users.noreply.github.com>
2020-07-22 16:25:44 +0300

    form

--- game.js
+++ game.js
@@ -2,14 +2,30 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    console.log(`${client.user.name}`);
-    console.log(`${client.user.displayName}`);
+    console.log(`Bot was started`);
 });
 
+let form = " \tИгра АПОКАЛИПСИС | НАЧАЛО ИГРЫ " +
+"\nНачните игру, нажми на реакцию!" +
+"\t\t\nУ тебя 15 секунд!";
+
+let playersCount = null;
+
 client.on('message', msg => {
-    if (msg.content === 'ping' || msg.content === 'пинг') {
-        msg.reply('понг');
+    if (msg.content === '!ИГРА')
+    {
+        gameChat = msg.channel;
+        gameChat.send(form);
+
+        // Create a reaction collector
+        const filter = (reaction, user) => reaction.emoji.name === '👌' && user.id === msg.author.id;
+        msg.react("👌");
+        const collector = msg.createReactionCollector(filter, { time: 15000 });
+        collector.on('collect', r => console.log(`Collected ${r.emoji.name}`));
+        collector.on('end', collected => {
+                gameChat.send(`Зарегистрированно ${collected.size} игроков.`);
+                playersCount = collected.size;
+            });
     }
 });
 
