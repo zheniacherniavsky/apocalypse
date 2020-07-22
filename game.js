71a21385025126ef1d5084e277d5f010e97ebfc5
HeavenlyRaccoon <vova55565@gmail.com>
2020-07-22 14:52:01 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1,24 +1,22 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    console.log(`${client.user.name}`);
-    console.log(`${client.user.displayName}`);
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`${client.user.name}`);
+  console.log(`${client.user.displayName}`);
 });
 
-client.on('message', msg => {
-    if (msg.content === 'урод' || msg.content === 'пинг') {
-        msg.reply('моральный');
-    }
+client.on("message", (msg) => {
+  if (msg.content === "урод" || msg.content === "пинг") {
+    msg.reply("моральный");
+  }
 });
 
-client.on('message', msg => {
-    if (msg.content === 'Леша' || msg.content === 'Устиненко') {
-        msg.reply('урод');
-    if (msg.content === 'жека') {
-        msg.reply('крутой');
-    }
+client.on("message", (msg) => {
+  if (msg.content === "хуй") {
+    msg.edit("сам хуй");
+  }
 });
 
-client.login('NzM1NDQ0OTkxODc0MTcwOTIx.XxgXNg.l1pJipDMPMfGtwoaNK0iObIQDU8');
+client.login("NzM1NDQ0OTkxODc0MTcwOTIx.XxgXNg.l1pJipDMPMfGtwoaNK0iObIQDU8");
