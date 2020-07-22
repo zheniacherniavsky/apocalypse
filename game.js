277524e165ab35990fba48d6657a2a756e880969
geshka <62078242+zheniacherniavsky@users.noreply.github.com>
2020-07-22 14:18:45 +0300

    Create game.js

---
+++ game.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`${client.user.name}`);
+    console.log(`${client.user.displayName}`);
+});
+
+client.on('message', msg => {
+    if (msg.content === 'ping' || msg.content === 'пинг') {
+        msg.reply('понг');
+    }
+});
+
+client.login('NzM1NDQ0OTkxODc0MTcwOTIx.XxgXNg.l1pJipDMPMfGtwoaNK0iObIQDU8');
