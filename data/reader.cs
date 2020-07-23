196d65daffcea561fe992cb9d9057e9a6e51dcc7
geshka <62078242+zheniacherniavsky@users.noreply.github.com>
2020-07-23 17:05:52 +0300

    data-arrays

---
+++ data/reader.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace DataReaderForApocalypse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string path_1 = @"null";
+            string path_1_1 = @"null";
+            
+            StreamReader r = new StreamReader(path_1, Encoding.Default);
+            StreamWriter w = new StreamWriter(path_1_1, false, Encoding.Default);
+            
+            string[] arr = new string[100];
+
+            string line;
+            bool finded;
+            
+            // 2
+            for (int i = 0; (line = r.ReadLine()) != null;)
+            {
+                finded = false;
+                line = line.Replace(";", ",");
+
+                foreach (var simillar in arr)
+                {
+                    if (simillar == line) finded = true;
+                }
+
+                if (!finded)
+                {
+                    arr[i] = line;
+                    i++;
+                }
+            }
+
+            Array.Sort(arr);
+            
+            w.WriteLine("let characters = [");
+            foreach (var a in arr)
+            {
+                if (a != null)
+                {
+                    w.WriteLine($"\t{a}");
+                    Console.WriteLine(a);
+                }
+            }
+            w.WriteLine("\n]");
+            
+            Console.WriteLine("Done!");
+            
+            w.Close();
+        }
+    }
+}
